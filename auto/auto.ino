6e38e86e1cd80c386ff80687675475ba94fa8193
Antti Alho <antti.s.alho@gmail.com>
2018-10-08 14:17:50 +0300

    palautus

--- auto/auto.ino
+++ auto/auto.ino
@@ -17,6 +17,8 @@ const int buttonPin5 = 5;
 const int buttonPin6 = 6;
 const int buttonPin7 = 7;
 const int buttonPin8 = 8;
+const int metri = 3250;
+const int turnTime = 310;
 
 int matka = 0;
 int buttonState2 = 0;
@@ -27,14 +29,14 @@ int buttonState6 = 0;
 int buttonState7 = 0;
 int buttonState8 = 0;
 int button8Memory = 0;
+int programState = 0;
 int tormays = 0;
 int aika = 0;
 int aika2 = 0;
 int aika3 = 0;
-const int metri = 3250;
 
-void setup() 
-{ 
+void setup() { 
+  Serial.begin(9600);
   pinMode(buttonPin2, INPUT_PULLUP);
   pinMode(buttonPin3, INPUT_PULLUP);
   pinMode(buttonPin4, INPUT_PULLUP);
@@ -47,7 +49,6 @@ void setup()
   servo2.attach(10);
   servo1.write(90);
   servo2.write(90);
-  Serial.begin(9600);
   u8x8.begin();
   u8x8.setPowerSave(0);
   u8x8.setFont(u8x8_font_chroma48medium8_r);
@@ -62,20 +63,6 @@ void loop() {
   buttonState6 = digitalRead(buttonPin6);
   buttonState7 = digitalRead(buttonPin7);
   buttonState8 = digitalRead(buttonPin8);
-  /*u8x8.setCursor(0,0);
-  u8x8.print(buttonState2);
-  u8x8.setCursor(2,0);
-  u8x8.print(buttonState3);
-  u8x8.setCursor(4,0);
-  u8x8.print(buttonState4);
-  u8x8.setCursor(6,0);
-  u8x8.print(buttonState5);
-  u8x8.setCursor(0,1);
-  u8x8.print(buttonState6);
-  u8x8.setCursor(2,1);
-  u8x8.print(buttonState7);
-  u8x8.setCursor(4,1);
-  u8x8.print(buttonState8);*/
     
   if (buttonState8 == LOW) {
     aika = millis();
@@ -88,8 +75,16 @@ void loop() {
     }
   }
   if (button8Memory == 1) {
-    suoraan();
+    //s
+    if (programState == 1) {
+
+    } else if (programState == 2) {
+
+    } else if (programState == 3) {
+
+    } 
   } else {
+    programState = 0;
     reset();
   }
   if (buttonState7 == LOW || buttonState6 == LOW || buttonState5 == LOW) {
@@ -134,49 +129,6 @@ void loop() {
   }
 }
 
-/*
-    if (buttonState3 == HIGH) {
-    suoraan();
-  }
-    if (buttonState4 == HIGH) {
-    suoraan();
-  }
-    if (buttonState5 == HIGH) {
-    suoraan();
-  }
-    if (buttonState6 == HIGH) {
-    suoraan();
-  }
-    if (buttonState7 == HIGH) {
-    suoraan();
-  }
-}
-  
-if (buttonState8 == HIGH) {
-    if (button8Memory == 1) {
-      button8Memory = 0;
-      delay(1000);
-    } else {
-      button8Memory = 1;
-      delay(1000);
-    }
-  }
-  
-  if (button8Memory == 1) {
-    suoraan();
-  } else {
-    reset();
-  }
-  
-  if (buttonState2 == HIGH || buttonState3 == HIGH || buttonState4 == HIGH) {
-    kaanny_oikea();
-    tormays += 1;
-  } else if (buttonState5 == HIGH || buttonState6 == HIGH  || buttonState7 == HIGH) {
-    kaanny_vasen();
-    tormays += 1;
-  }
-  */
-
 void kaanny_oikea() {
   servo1.write(0);
   servo2.write(180);
@@ -195,5 +147,19 @@ void reset() {
   servo1.write(90);
   servo2.write(90);
 }
-//u8x8.setCursor(0,0);
-//u8x8.print(matka);
+
+void startCorner(){
+
+}
+
+void startMiddle(){
+
+}
+
+void drawPic(){
+
+}
+
+void start(){
+
+}
---
+++ tutka/tutka.ino
@@ -0,0 +1,304 @@
+#include <Time.h>
+#include <TimeLib.h>
+#include <U8x8lib.h>
+#include <U8g2lib.h>
+#include <Servo.h> 
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+
+U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE); 
+Servo servo1;
+Servo servo2;
+Servo echo;
+const int buttonPin2 = 2;
+const int buttonPin3 = 3;
+const int buttonPin4 = 4;
+const int buttonPin5 = 5;
+const int buttonPin6 = 6;
+const int buttonPin7 = 7;
+const int buttonPin8 = 8;
+const int trigPin = 13;
+const int echoPin = 12;
+const int metri = 3250;
+const int turnTime = 290;
+
+float area = 0;
+int buttonState2 = 0;
+int buttonState3 = 0;
+int buttonState4 = 0;
+int buttonState5 = 0;
+int buttonState6 = 0;
+int buttonState7 = 0;
+int buttonState8 = 0;
+int button8Memory = 0;
+int programState = 0;
+int tormays = 0;
+int aika = 0;
+int aika2 = 0;
+int aika3 = 0;
+
+uint8_t tiles0[8] = {
+0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+};
+uint8_t tiles1[8] = {
+0xff,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+};
+uint8_t tiles2[8] = {
+0xff,0xff,0x00,0x00,0x00,0x00,0x00,0x00
+};
+uint8_t tiles3[8] = {
+0xff,0xff,0xff,0x00,0x00,0x00,0x00,0x00
+};
+uint8_t tiles4[8] = {
+0xff,0xff,0xff,0xff,0x00,0x00,0x00,0x00
+};
+uint8_t tiles5[8] = {
+0xff,0xff,0xff,0xff,0xff,0x00,0x00,0x00
+};
+uint8_t tiles6[8] = {
+0xff,0xff,0xff,0xff,0xff,0xff,0x00,0x00
+};
+uint8_t tiles7[8] = {
+0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x00
+};
+uint8_t tiles8[8] = {
+0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff
+};
+
+uint8_t tiles[10][8] =  {
+  {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+  {0xff,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+  {0xff,0xff,0x00,0x00,0x00,0x00,0x00,0x00},
+  {0xff,0xff,0xff,0x00,0x00,0x00,0x00,0x00},
+  {0xff,0xff,0xff,0xff,0x00,0x00,0x00,0x00},
+  {0xff,0xff,0xff,0xff,0xff,0x00,0x00,0x00},
+  {0xff,0xff,0xff,0xff,0xff,0xff,0x00,0x00},
+  {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0x00},
+  {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff},
+  {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff},
+};
+long duration = 0;
+int distance = 0;
+
+void setup() { 
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(buttonPin3, INPUT_PULLUP);
+  pinMode(buttonPin4, INPUT_PULLUP);
+  pinMode(buttonPin5, INPUT_PULLUP);
+  pinMode(buttonPin6, INPUT_PULLUP);
+  pinMode(buttonPin7, INPUT_PULLUP);
+  pinMode(buttonPin8, INPUT_PULLUP);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
+  servo1.attach(9);
+  servo2.attach(10);
+  echo.attach(11);
+  
+  Serial.begin(9600);
+  
+  servo1.write(90);
+  servo2.write(90);
+  echo.write(90);
+  u8x8.begin();
+  u8x8.setPowerSave(0);
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  setTime(0);
+} 
+
+void loop() {
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
+  buttonState4 = digitalRead(buttonPin4);
+  buttonState5 = digitalRead(buttonPin5);
+  buttonState6 = digitalRead(buttonPin6);
+  buttonState7 = digitalRead(buttonPin7);
+  buttonState8 = digitalRead(buttonPin8);
+
+  //choose what to do
+  if (buttonState2 == LOW) {
+    u8x8.setCursor(0,2);
+    u8x8.print("progstate = 1");
+    programState = 1;
+  } else if (buttonState3 == LOW) {
+    u8x8.setCursor(0,2);
+    u8x8.print("progstate = 2");
+    programState = 2;
+  } else if (buttonState4 == LOW) {
+    u8x8.setCursor(0,2);
+    u8x8.print("progstate = 3");
+    programState = 3;      
+  }
+  
+  // start chosen program 
+  if (buttonState8 == LOW) {
+    aika = millis();
+    if (button8Memory == 1) {
+      button8Memory = 0;
+      u8x8.setCursor(0,1);
+      u8x8.print("mem8 = 0");
+      delay(500);
+    } else {
+      button8Memory = 1;
+      u8x8.setCursor(0,1);
+      u8x8.print("mem8 = 1");
+      delay(500);
+    }
+  }
+  if (button8Memory == 1) {   
+    if (programState == 1) {
+      startCorner();
+    } else if (programState == 2) {
+      reset();
+    } else if (programState == 3) {
+      drawPic();
+    } 
+  }
+}
+
+void kaanny_oikea() {
+  servo1.write(0);
+  servo2.write(180);
+  delay(290);
+}
+void kaanny_vasen() {
+  servo2.write(0);
+  servo1.write(180);
+  delay(290);
+}
+void suoraan() {
+  servo1.write(180);
+  servo2.write(180);
+}
+void reset() {
+  servo1.write(90);
+  servo2.write(90);
+}
+
+void startCorner(){
+  int collisiontime1 = 0;
+  int collisiontime2 = 0;
+  suoraan();
+  while (programState == 1) {
+    buttonState2 = digitalRead(buttonPin2);
+    buttonState3 = digitalRead(buttonPin3);
+    buttonState4 = digitalRead(buttonPin4);
+    buttonState5 = digitalRead(buttonPin5);
+    buttonState6 = digitalRead(buttonPin6);
+    buttonState7 = digitalRead(buttonPin7);
+    buttonState8 = digitalRead(buttonPin8);
+    u8x8.setCursor(0,3);
+    u8x8.print("inprogram loop");
+    if (buttonState7 == LOW || buttonState6 == LOW || buttonState5 == LOW) {
+      kaanny_oikea();
+      suoraan();
+      tormays += 1;
+      if (tormays == 1){
+        collisiontime1 = millis();
+        setTime(0);
+      } else if (tormays == 2) {
+        collisiontime2 = millis();
+      }
+    }
+    if (buttonState2 == LOW || buttonState3 == LOW || buttonState4 == LOW) {
+      kaanny_vasen();
+      suoraan();
+      tormays += 1;
+      if (tormays == 1){
+        collisiontime1 = millis();
+        setTime(0);
+      } else if (tormays == 2) {
+        collisiontime2 = millis();
+      }
+    }
+    
+    if (tormays == 2) {
+      button8Memory = 0;
+      reset();
+      aika2 = millis();
+      aika3 = (aika2 - aika) - 580;
+      area = (float)((float)collisiontime1/metri)*((float)collisiontime2/metri);
+      u8x8.setCursor(0,0);
+      u8x8.print(area);
+      programState = 0;
+    }
+  }
+}
+
+void drawPic(){
+  int dist1 = 0;
+  int dist2 = 0;
+  suoraan();
+  u8x8.setCursor(0,1);
+  u8x8.print("                ");
+  u8x8.setCursor(0,2);
+  u8x8.print("                ");
+  while(programState == 3){
+    buttonState2 = digitalRead(buttonPin2);
+    buttonState3 = digitalRead(buttonPin3);
+    buttonState4 = digitalRead(buttonPin4);
+    buttonState5 = digitalRead(buttonPin5);
+    buttonState6 = digitalRead(buttonPin6);
+    buttonState7 = digitalRead(buttonPin7);
+    buttonState8 = digitalRead(buttonPin8);
+    draw(calcdistance());
+    if (buttonState7 == LOW || buttonState6 == LOW || buttonState5 == LOW) {
+      kaanny_oikea();
+      suoraan();
+    }
+    if (buttonState2 == LOW || buttonState3 == LOW || buttonState4 == LOW) {
+      kaanny_vasen();
+      suoraan();
+    }
+    if (calcdistance() < 20) {
+      reset();
+      echo.write(0);
+      delay(300);
+      dist1 = calcdistance();
+      echo.write(180);
+      delay(300);
+      dist2 = calcdistance();
+      echo.write(90);
+      delay(300);
+      if (dist1 >= dist2) {
+        kaanny_oikea();
+        suoraan();
+      } else {
+        kaanny_vasen();
+        suoraan();
+      }
+    } else {
+      suoraan();
+    }
+  }
+}
+
+void draw(int a) {
+  int b = a/10;
+  int c = a%10;
+  int x = 0;
+  u8x8.setCursor(0,1);
+  u8x8.print(b);
+  u8x8.setCursor(0,2);
+  u8x8.print(c);
+  u8x8.setCursor(0,0);
+  u8x8.print("                ");
+  for (x; x<b; x++ ){
+    u8x8.drawTile(x, 0, 1, tiles[8]);
+  }
+  u8x8.drawTile(b, 0, 1, tiles[c]);
+}
+
+int calcdistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin,LOW);
+  duration = pulseIn(echoPin,HIGH);
+  distance = duration*0.034/2;
+  u8x8.setCursor(0,3);
+  u8x8.print("dist:");
+  u8x8.print(distance);
+  return distance;
+}
